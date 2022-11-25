44d188787df477739e43c9be005f23bd308b4105
Javier Rivera <rf21001ts@esfe.agape.edu.sv>
2022-11-24 19:01:43 -0600

    cambio en stream de conexion

--- Clinica.AdministradorBlazor/Program.cs
+++ Clinica.AdministradorBlazor/Program.cs
@@ -10,8 +10,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44371/api/") });
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://www.deptossv.somee.com/api/") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44371/api/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(" https://www.clinicasv.somee.com/api/") });
 
 
 builder.Services.AddScoped<UserAuth>();
