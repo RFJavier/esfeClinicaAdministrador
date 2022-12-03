27061b74d0871b508db28fc815f8d1fcb7c1541b
Javier Rivera <rf21001ts@esfe.agape.edu.sv>
2022-12-03 17:12:12 -0600

    arreglar problemas con los label de la vista paciente/agregar

---
+++ Clinica.AdministradorBlazor/Model/Contraseña.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Clinica.AdministradorBlazor.Model
+{
+    public class Contraseña
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Rol")]
+        [Required(ErrorMessage = "Rol es obligatorio")]
+        [Display(Name = "Rol")]
+        public int IdRol { get; set; }
+
+        [Required(ErrorMessage = "Login es obligatorio")]
+        [StringLength(25, ErrorMessage = "Maximo 25 caracteres")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Password es obligatorio")]
+        [DataType(DataType.Password)]
+        [StringLength(32, ErrorMessage = "Password debe estar entre 5 a 32 caracteres", MinimumLength = 5)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Estatus es obligatorio")]
+        public byte Estatus { get; set; }
+
+        public Roles Rol { get; set; }
+    }
+}
